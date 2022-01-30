ecf2f76991095862055ae89427c34ddca022100e
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-01-30 11:03:10 +0000

    P3 Original

---
+++ p3original.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int input()
+{
+ int n;
+ printf("Enter a Number\n");
+ scanf("%d",&n);
+ return n;
+}
+int sum_n(int n)
+{
+  int i,sum=0;
+ for(i=1;i<=n;i++)
+ {
+   sum=sum+i;
+ }
+ return sum;
+}
+void output(int n,int sum)
+{
+ for(int i=1;i<=n;i++)
+ {
+   printf("%d+ ",i);
+ }
+ printf("\bis %d \n",sum);
+}
+int main()
+{
+ int x,y;
+ x=input();
+ y=sum_n(x);
+ output(x,y);
+ return 0;
+}
