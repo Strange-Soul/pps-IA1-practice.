b1efedfe51b8d4a9d64bd27a8be086e917250eeb
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-02-02 02:17:15 +0000

    P4 original

---
+++ p4original.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int input_array_size()
+{
+ int n;
+ printf("Enter the size of array\n");
+ scanf("%d",&n);
+ return n;
+}
+void input_array(int n,int a[n])
+{
+printf("Enter array Element\n");
+for(int i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+}
+}
+int sum_n_arrays(int n,int a[n])
+{
+ int i,sum=0;
+ for(i=0;i<n;i++)
+ {
+   sum=sum+a[i];
+ }
+ return sum;
+}
+void out_put(int n,int a[n],int sum)
+{
+  for(int i=0;i<n;i++)
+  {
+    printf("%d+",a[i]);
+  }
+  printf("\bis %d\n",sum);
+}
+int main()
+{
+  int x,z;
+  x=input_array_size();
+  int y[x];
+  input_array(x,y);
+  z=sum_n_arrays(x,y);
+  out_put(x,y,z);
+  return 0;
+}
