1e69f0157ae05bb9825abac2c44a4466d67ea0c5
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-02-02 05:01:01 +0000

    P3 Original

--- p3original.c
+++ p3original.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-int input()
+int input_n()
 {
  int n;
- printf("Enter a Number\n");
+ printf("Enter a Number to Add \n");
  scanf("%d",&n);
  return n;
 }
 int sum_n(int n)
 {
-  int i,sum=0;
+ int i,sum=0;
  for(i=1;i<=n;i++)
  {
    sum=sum+i;
@@ -17,7 +17,8 @@ int sum_n(int n)
 }
 void output(int n,int sum)
 {
- for(int i=1;i<=n;i++)
+ int i;
+ for(i=1;i<=n;i++)
  {
    printf("%d+",i);
  }
@@ -26,7 +27,7 @@ void output(int n,int sum)
 int main()
 {
  int x,y;
- x=input();
+ x=input_n();
  y=sum_n(x);
  output(x,y);
  return 0;
