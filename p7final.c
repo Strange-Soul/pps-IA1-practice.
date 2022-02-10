43fdb50c0aef06a91035c229c0eca88e1ae5bb9e
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-02-10 07:02:07 +0000

    P7 Final

---
+++ p7final.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+struct _complex
+{
+  float real,imaginary;
+};
+
+typedef struct _complex Complex;
+
+Complex input_complex()
+{
+ Complex z;
+ printf("Enter a complex number \n");
+ scanf("%f %f",&z.real,&z.imaginary);
+ return z;
+}
+Complex add(Complex a,Complex b)
+{
+ Complex s;
+ s.real=a.real+b.real;
+ s.imaginary=a.imaginary=b.imaginary;
+ return s;
+}
+void output(Complex a,Complex b,Complex c)
+{
+ printf("%f+%fi  +  %f+%fi is %f+%fi \n",a.real,a.imaginary,b.real,b.imaginary,c.real,c.imaginary);
+}
+int main()
+{
+ Complex x,y,z;
+ x=input_complex();
+ y=input_complex();
+ z=add(x,y);
+ output(x,y,z);
+ return 0;
+}
